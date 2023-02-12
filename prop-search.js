7100da910f0b2c2e7ea482ecaf02aeba4e72bf94
ryohey <ryohey0405@gmail.com>
2023-02-12 22:42:04 +0900

    a

--- prop-search.js
+++ prop-search.js
@@ -1,6 +1,5 @@
 
-//プロパティ格納divに名前をつける
-
+//プロパティの連想配列を作成する。
 var prop_object = {};
 const prop_wrap = document.querySelectorAll('.notion-page__property');
 prop_wrap.forEach(element => {
@@ -8,9 +7,9 @@ prop_wrap.forEach(element => {
     const prop_value = element.getElementsByTagName("span")[1].getElementsByTagName("span")[0].innerHTML;
     console.log(prop_name); 
     console.log(prop_value); 
-    prop_object.prop_name = prop_value;
+    prop_object[prop_name] = prop_value;
 });
-
 console.log(prop_object);
 
 
+///ページ名によって読み込むJSを切り替える
