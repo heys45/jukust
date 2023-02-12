c652753983307b188bdcf1f03bb6f6c2e9b46faa
ryohey <ryohey0405@gmail.com>
2023-02-12 21:51:05 +0900

    prop-サーチ

---
+++ prop-search.js
@@ -0,0 +1,17 @@
+function prop-search(){
+//プロパティ格納divに名前をつける
+const prop_wrap = document.querySelectorAll('.notion-page__property-name');
+
+prop_wrap.forEach(index,element => {
+    console.log(index);
+    console.log(element);
+    
+});
+
+
+//var span2 = pageproperty.getElementsByTagName("span")[1];
+//var span3 = span2.getElementsByTagName("span")[0].innerHTML;
+//console.log(span3);
+
+}
+
