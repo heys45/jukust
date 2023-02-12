bc1dafd06df0d88a8b0e1da73ab1ea68236b7a85
ryohey <ryohey0405@gmail.com>
2023-02-13 01:46:07 +0900

    a

--- prop-search.js
+++ prop-search.js
@@ -32,8 +32,8 @@ groups.forEach(element =>{
     var id = element.getElementsByTagName("span")[1].innerHTML;
     element.setAttribute("id",id);
 });
-console.log(prop_object["教室ID"]);
-var test = document.getElementById(prop_object["教室ID"]);
+console.log(prop_object["講師ID"]);
+var test = document.getElementById(prop_object["講師ID"]);
 console.log(test);
 test.style.display = "block" ;
 
