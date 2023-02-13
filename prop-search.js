44ea7d3b7e92d13df6ee0f8165157e74e5f69574
ryohey <ryohey0405@gmail.com>
2023-02-13 16:42:01 +0900

    a

--- prop-search.js
+++ prop-search.js
@@ -1,33 +1,60 @@
+// 2. for文で配列をループ処理する例
+let fruits = ["apple", "orange", "melon"];
+
+for(let i=0 ; i< fruits.length; i++){
+  console.log( fruits[i] );
+}
+
+
 
 //プロパティの連想配列を作成する。
 var prop_object = {};
 const prop_wrap = document.querySelectorAll('.notion-page__property');
-prop_wrap.forEach(element => {
-    const prop_name = element.getElementsByTagName("span")[0].innerHTML;
-    const prop_value = element.getElementsByTagName("span")[1].getElementsByTagName("span")[0].innerHTML;
+
+for(let i=0; i< prop_wrap.length; i++){
+    const prop_name = prop_wrap[i].getElementsByTagName("span")[0].innerHTML;
+    const prop_value = prop_wrap[i].getElementsByTagName("span")[1].getElementsByTagName("span")[0].innerHTML;
     console.log(prop_name); 
     console.log(prop_value); 
     prop_object[prop_name] = prop_value;
-});
+}
 console.log(prop_object);
+
+
+// prop_wrap.forEach(element => {
+//     const prop_name = element.getElementsByTagName("span")[0].innerHTML;
+//     const prop_value = element.getElementsByTagName("span")[1].getElementsByTagName("span")[0].innerHTML;
+//     console.log(prop_name); 
+//     console.log(prop_value); 
+//     prop_object[prop_name] = prop_value;
+// });
+// console.log(prop_object);
 
 // テキストブロックの連想配列を作成する。
 var qblock_object = {};
 const qblock_wrap = document.querySelectorAll('.notion-quote');
-qblock_wrap.forEach(element => {
-    const qblock_name = element.getElementsByTagName("strong")[0].innerHTML;
-    element.setAttribute("id",qblock_name);
+
+for(let i=0; i< qblock_wrap.length; i++){
+    const qblock_name = qblock_wrap[i].getElementsByTagName("strong")[0].innerHTML;
+    qblock_wrap[i].setAttribute("id",qblock_name);
     const qblock_value = document.getElementById(qblock_name);
     console.log(qblock_name); 
     console.log(qblock_value); 
     qblock_object[qblock_name] = qblock_value;
-    var test = document.getElementById("シフト申請はこちら");
-    console.log(test);
-});
-console.log(qblock_object);
+}
+console.log(prop_object);
 
-var test = document.getElementById("シフト申請はこちら");
-console.log(test);
+// qblock_wrap.forEach(element => {
+//     const qblock_name = element.getElementsByTagName("strong")[0].innerHTML;
+//     element.setAttribute("id",qblock_name);
+//     const qblock_value = document.getElementById(qblock_name);
+//     console.log(qblock_name); 
+//     console.log(qblock_value); 
+//     qblock_object[qblock_name] = qblock_value;
+//     var test = document.getElementById("シフト申請はこちら");
+//     console.log(test);
+// });
+// console.log(qblock_object);
 
 
 ///ページ名によって読み込むHTML要素とJSを切り替える
