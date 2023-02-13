f836c6b8c347bd5eded62de3273febfc53e133f1
ryohey <ryohey0405@gmail.com>
2023-02-14 00:57:49 +0900

    あ

--- page-schedule.js
+++ page-schedule.js
@@ -18,7 +18,7 @@ if(prop_object["ページ表示名"]==="スケジュール提出"){
 
     // フォームを指定したテキストブロック要素の後に挿入
     var form_area = document.getElementById("form-area");
-    var quote_name = "シフト申請はこちら";
+    var quote_name = "シフト申請はこちらから";
     document.getElementById("quote_name").after();
 
 
