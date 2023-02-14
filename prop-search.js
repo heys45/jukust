e2b8d6c74dd9c2577abc3b53beb8743c009d7d06
ryohey <ryohey0405@gmail.com>
2023-02-14 09:55:40 +0900

    a

--- prop-search.js
+++ prop-search.js
@@ -1,11 +1,6 @@
-var prop_object = {};
-var qblock_object = {};
-
-
 //プロパティの連想配列を作成する。
-// var prop_object = {};
+var prop_object = {};
 const prop_wrap = document.querySelectorAll('.notion-page__property');
-
 for (let i=0; i< prop_wrap.length; i++){
     const prop_name = prop_wrap[i].getElementsByTagName("span")[0].innerHTML;
     const prop_value = prop_wrap[i].getElementsByTagName("span")[1].getElementsByTagName("span")[0].innerHTML;
@@ -15,19 +10,9 @@ for (let i=0; i< prop_wrap.length; i++){
 }
 console.log(prop_object);
 
-// prop_wrap.forEach(element => {
-//     const prop_name = element.getElementsByTagName("span")[0].innerHTML;
-//     const prop_value = element.getElementsByTagName("span")[1].getElementsByTagName("span")[0].innerHTML;
-//     console.log(prop_name); 
-//     console.log(prop_value); 
-//     prop_object[prop_name] = prop_value;
-// });
-// console.log(prop_object);
-
 // テキストブロックの連想配列を作成する。
-// var qblock_object = {};
+var qblock_object = {};
 const qblock_wrap = document.querySelectorAll('.notion-quote');
-
 for (let i=0; i< qblock_wrap.length; i++){
     const qblock_name = qblock_wrap[i].getElementsByTagName("strong")[0].innerHTML;
     qblock_wrap[i].setAttribute("id",qblock_name);
@@ -36,26 +21,18 @@ for (let i=0; i< qblock_wrap.length; i++){
     console.log(qblock_value); 
     qblock_object[qblock_name] = qblock_value;
 }
-
-
 console.log(qblock_object);
 
-var testw = "提出スケジュールの現状ステータス"; 
-testa = document.getElementById(testw);
-console.log(testa);
 
 
-// qblock_wrap.forEach(element => {
-//     const qblock_name = element.getElementsByTagName("strong")[0].innerHTML;
-//     element.setAttribute("id",qblock_name);
-//     const qblock_value = document.getElementById(qblock_name);
-//     console.log(qblock_name); 
-//     console.log(qblock_value); 
-//     qblock_object[qblock_name] = qblock_value;
-//     var test = document.getElementById("シフト申請はこちら");
-//     console.log(test);
-// });
-// console.log(qblock_object);
+
+
+
+
+
+
+
+
 
 
 ///ページ名によって読み込むHTML要素とJSを切り替える
@@ -105,3 +82,7 @@ trigger.onchange=viewChange;
 
 // フォーム＝シフトの時
 
+console.log(prop_object["講師ID"]);
+var footer = document.getElementsByTagName("footer");
+var main_content = document.getElementsByClassName("super-content")[0];
+main_content.after(footer) ;
