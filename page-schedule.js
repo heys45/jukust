fc44c33a6153891f65aaf3136769162a3bf2f574
ryohey <ryohey0405@gmail.com>
2023-02-14 12:12:16 +0900

    a

--- page-schedule.js
+++ page-schedule.js
@@ -18,7 +18,7 @@ if(prop_object["ページ表示名"]==="スケジュール提出"){
     console.log(teacher_id);
     var teacher_class ="."+teacher_id
     console.log(teacher_class);
-    var test = document.querySelectorAll(teacher_class);
+    var test = document.getElementsByClassName(prop_object["講師ID"]);
     test.forEach(element =>{
     element.style.display = "block" ;
     });
