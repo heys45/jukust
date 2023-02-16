2295ed20a1e86e7620c0fe40deb0be1785a181d8
ryohey <ryohey0405@gmail.com>
2023-02-16 15:43:21 +0900

    a

--- page-schedule.js
+++ page-schedule.js
@@ -208,26 +208,70 @@ if(prop_object["ページ表示名"]==="スケジュール提出"){
 
 
 
-      //テーブルをまとめて取得
-      const tables = document.querySelectorAll('.notion-table');
-      for( var i=0; i<tables.length; i++) {
-          var name = "table-"+(i+1);
-          tables[i].setAttribute("id",name);
-          //行をまとめて取得
-          const rows = tables[i].getElementsByTagName("tr")
-          //行にIDを付与する 
-          for( var j=0; j<rows.length; j++) {
-              var name = "table-"+(i+1)+"-"+(j+1);
-              rows[j].setAttribute("id",name);
-              //列をまとめて取得
-              const columns =rows[j].getElementsByTagName("td")
-              //列にIDを付与する 
-              for( var k=0; k<columns.length; k++) {
-                  var name = "table-"+(i+1)+"-"+(j+1)+"-"+(k+1);
-                  columns[k].setAttribute("id",name);
-              }
-          }
-      }
+      // //テーブルをまとめて取得
+      // const tables = document.querySelectorAll('.notion-table');
+      // for( var i=0; i<tables.length; i++) {
+      //     var name = "table-"+(i+1);
+      //     tables[i].setAttribute("id",name);
+      //     //行をまとめて取得
+      //     const rows = tables[i].getElementsByTagName("tr")
+      //     //行にIDを付与する 
+      //     for( var j=0; j<rows.length; j++) {
+      //         var name = "table-"+(i+1)+"-"+(j+1);
+      //         rows[j].setAttribute("id",name);
+      //         //列をまとめて取得
+      //         const columns =rows[j].getElementsByTagName("td")
+      //         //列にIDを付与する 
+      //         for( var k=0; k<columns.length; k++) {
+      //             var name = "table-"+(i+1)+"-"+(j+1)+"-"+(k+1);
+      //             columns[k].setAttribute("id",name);
+      //         }
+      //     }
+      // }
+
+
+
+
+//テーブルをまとめて取得
+const tables = document.querySelectorAll('.notion-table');
+//テーブルにIDを付与する 
+for( var i=0; i<tables.length; i++) {
+  console.log(tables[i]);
+  var name = "table-"+(i+1);
+  console.log(name);
+  tables[i].setAttribute("id",name);
+  
+  //行をまとめて取得
+  const rows = tables[i].getElementsByTagName("tr")
+  //行にIDを付与する 
+  for( var j=0; j<rows.length; j++) {
+  console.log(rows[j]);
+  var name = "table-"+(i+1)+"-"+(j+1);
+  console.log(name);
+  rows[j].setAttribute("id",name);
+  
+  //列をまとめて取得
+  const columns =rows[j].getElementsByTagName("td")
+  //列にIDを付与する 
+  for( var k=0; k<columns.length; k++) {
+  console.log(columns[k]);
+  var name = "table-"+(i+1)+"-"+(j+1)+"-"+(k+1);
+  console.log(name);
+  columns[k].setAttribute("id",name);
+
+  }
+  }
+}
+
+
+//書き換え処理
+var div = document.getElementById('table-1-1-3');
+var span =div.getElementsByTagName("span")[0];
+span.innerHTML = "<span>サンプルタイトル</span>";
+
+
+
+
       
       //書き換え処理
       var div = document.getElementById('table-1-1-3');
