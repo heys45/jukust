5c3dbc81c1aff10b582619494ddb869156ddadc9
ryohey <ryohey0405@gmail.com>
2023-02-17 17:34:45 +0900

    a

--- page-schedule.js
+++ page-schedule.js
@@ -230,6 +230,7 @@ if(prop_object["ページ表示名"]==="スケジュール提出"){
 
     sc_ft1s.forEach(element=>{
       console.log(element);
+      element.style.display ="none";
       element.style.backgroundColor = "#999999";
     });
     sc_ft2s .forEach(element=>{
