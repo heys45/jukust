6edacbb4c9d003f306d53e8ddd4c47fb373ec2b6
ryohey <ryohey0405@gmail.com>
2023-02-22 00:16:33 +0900

    a

--- page-request.js
+++ page-request.js
@@ -350,12 +350,12 @@ console.log(main_db);
 
 
 //フォーム送信後の処理
-var shift_form_btn = document.getElementById("schedule-form-btn");
-shift_form_btn.addEventListener('click', shift_form);
-function shift_form() {
-  document.getElementById('shift-form-comp').style.display = 'block';
-  shift_form_btn.style["background-color"] ="gray";
-  }
+// var shift_form_btn = document.getElementById("schedule-form-btn");
+// shift_form_btn.addEventListener('click', shift_form);
+// function shift_form() {
+//   document.getElementById('shift-form-comp').style.display = 'block';
+//   shift_form_btn.style["background-color"] ="gray";
+//   }
 }}
 
 
