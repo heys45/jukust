899906fc090618f7894714581a04c6e6d9bcdf3a
ryohey <ryohey0405@gmail.com>
2023-02-22 13:04:58 +0900

    a

--- page-request.js
+++ page-request.js
@@ -350,7 +350,7 @@ var main_db ={};
       a_db_data[index].appendChild(nullspan);
       var a_db_data_n = a_db_data[index].getElementsByTagName("span")[0];
       var checker =console.log(a_db_data_n.getElementsByTagName("span")[0] !=null);
-      if (checker===true) {// spanが二段階の時
+      if (checker == true) {// spanが二段階の時
         var a_db_data_n = a_db_data_n.getElementsByTagName("span")[0];
         var a_db_data_n =  a_db_data_n.innerHTML;
         main_db[db_header[index].innerHTML].push(a_db_data_n);
