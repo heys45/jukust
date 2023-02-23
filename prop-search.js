b8195ef032d3a78436d3db61930231e131ea1179
ryohey <ryohey0405@gmail.com>
2023-02-23 15:05:38 +0900

    a

--- prop-search.js
+++ prop-search.js
@@ -22,8 +22,10 @@ const prop_wrap = document.querySelectorAll('.notion-page__property');
 for (let i=0; i< prop_wrap.length; i++){
     const prop_name = prop_wrap[i].getElementsByTagName("span")[0].innerHTML;
     var prop_value = null;
-    console.log(prop_wrap[i].getElementsByTagName("span")[1].getElementsByTagName("span")[0] !=null);
-    if(prop_wrap[i].getElementsByTagName("span")[1].getElementsByTagName("span")[0] !=null){
+    var check = prop_wrap[i].getElementsByTagName("span")[1]
+    console.log(check !=null);
+    console.log(check.getElementsByTagName("span")[0] !=null);
+    if(check.getElementsByTagName("span")[0] !=null){
     var prop_value = prop_wrap[i].getElementsByTagName("span")[1].getElementsByTagName("span")[0].innerHTML;
     }
     console.log(prop_name); 
