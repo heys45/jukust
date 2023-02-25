df94f99d0c000c67b4c3430bb048380aa624b464
ryohey <ryohey0405@gmail.com>
2023-02-25 13:02:42 +0900

    a

--- page-request.js
+++ page-request.js
@@ -285,8 +285,10 @@ for( var i=0; i<dbs.length; i++) {
   const rows = dbs[i].getElementsByTagName("tr")
   //行にIDを付与する 
   for( var j=0; j<rows.length; j++) {
+  if(j != 0){
   var name = rows[j].getElementsByTagName("span")[0].getElementsByTagName("span")[0].innerHTML;
   rows[j].setAttribute("name",name);
+  }
   var name = "db-"+(i+1)+"-"+(j+1);
   rows[j].setAttribute("id",name);
   
@@ -366,6 +368,49 @@ death_fd.remove();
 //   document.getElementById('shift-form-comp').style.display = 'block';
 //   shift_form_btn.style["background-color"] ="gray";
 //   }
+
+// 日付部分
+console.log(main_db["日付"]);
+main_db["日付"].forEach((element,index)=>{
+  var counter = 0;
+  console.log(main_db["報告フラグ"]);
+  if(main_db["報告フラグ"][index]=1){
+    // フォーマットの複製
+    var target =document.getElementsByClassName("wr-formbox")[counter];
+    var clone  = target.cloneNode(true);
+    target.after(clone);
+
+    // 出勤フォーマットにフォーム挿入
+    var formset = document.getElementsByClassName("in-dg")[counter];
+    target.getElementsByClassName("in-content")[0].appendChild(formset);
+    // 出勤フォーマットにデータセット
+    target.getElementsByClassName("in-day-label")[0].textContent = element;
+
+    var target_class="in-fd" ; var textcontent = "実際の出勤時間";
+    spancontent.textContent = textcontent;
+    var spancontent =document.create("span");
+    target.getElementsByClassName(target_class)[0].before(spancontent);
+
+
+
+    // 退勤フォーマットにフォーム挿入
+    var formset = document.getElementsByClassName("out-dg")[counter];
+    target.getElementsByClassName("out-content")[0].appendChild(formset);
+    // 退勤フォーマットにデータセット
+    target.getElementsByClassName("out-day-label")[0].textConten= element;
+
+    // カウントアップ
+    var counter = counter+1;
+
+  }
+})
+
+
+
+
+
+
+
 
 }}
 
