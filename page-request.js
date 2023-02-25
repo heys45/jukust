f2672ae28d17bfb2757b708570cda38a1bd64b78
ryohey <ryohey0405@gmail.com>
2023-02-25 15:21:53 +0900

    a

--- page-request.js
+++ page-request.js
@@ -405,7 +405,7 @@ console.log(main_db);
 
       var sch_info = document.getElementById(sch_id).getElementsByTagName("span")[0].innerHTML;
       target.getElementsByClassName("sch-info")[0].innerHTML = sch_info;
-    }else{target.getElementsByClassName("sch-info")[0].remove;}
+    }else{target.getElementsByClassName("sch-info")[0].remove();}
 
     // フォームを入れる
     target.getElementsByClassName("day-box-form")[0].appendChild(element);
