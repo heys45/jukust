ae274dc8f3c795bbbaabe9e5036e6c1cf8873060
ryohey <ryohey0405@gmail.com>
2023-02-27 13:02:14 +0900

    a

--- page-schedule.js
+++ page-schedule.js
@@ -312,8 +312,8 @@ forms.forEach((element,index) =>{
 
   var formss = document.querySelectorAll(".day-box");
   formss.forEach(element =>{
-    // var restart = element.getElementsByClassName("ft1")[0].value;
-    console.log(element.getElementsByClassName("ft1")[0]);
+    var restart = element.getElementsByClassName("ft1")[0].value;
+    console.log(element.getElementsByClassName("ft1")[0]).value;
     if(restart !=null){
       var reday = element.getElementsByClassName("fd")[0].value;
       var reend = element.getElementsByClassName("ft2")[0].value;
