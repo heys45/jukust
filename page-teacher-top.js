3a4e2ba8af6a5ffd6126a793d677aad0c0ba75c4
ryohey <ryohey0405@gmail.com>
2023-02-28 14:57:11 +0900

    a

--- page-teacher-top.js
+++ page-teacher-top.js
@@ -318,10 +318,27 @@ console.log(senkou_db);
 senkou_db["TimeRex入力済"].forEach((element,index)=>{
   if(senkou_db["TimeRex入力済"][index]==1){
     var area_target = document.getElementById("やることリスト").nextElementSibling;
-    var TimeRexbox = document.createElement("div")
-    TimeRexbox.innerHTML='<div id="timerex_calendar" data-url="https://timerex.net/s/kaitoyoshitomi_4a1b/7ba6055f"></div><script id="timerex_embed" src="https://asset.timerex.net/js/embed.js"></script><script type="text/javascript">TimerexCalendar();</script>';
+
+    var TimeRexbox = document.createElement("div");
+    TimeRexbox.setAttribute("id","TRB")
     area_target.after(TimeRexbox);
-    TimeRexbox.style.display="block";
+    area_target = document.getElementById("TRB");
+
+    var TimeRex = document.createElement("div");
+    TimeRex.setAttribute("id","timerex_calender");
+    TimeRex.setAttribute("data-url","https://timerex.net/s/kaitoyoshitomi_4a1b/7ba6055f");
+    area_target.appendChild(TimeRex);
+
+    var TimeRex = document.createElement("script");
+    TimeRex.setAttribute("id","timerex_embed");
+    TimeRex.setAttribute("src","https://asset.timerex.net/js/embed.js");
+    area_target.appendChild(TimeRex);
+
+    var TimeRex = document.createElement("script");
+    TimeRex.setAttribute("type","text/javascript");
+    TimeRex.innerHTML="TimerexCalendar();"
+    area_target.appendChild(TimeRex);
+
   };
 });
 
