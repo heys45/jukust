762ad3986ab286732c75a23338b227d3ab6ce495
ryohey <ryohey0405@gmail.com>
2023-02-28 18:47:16 +0900

    あ

--- page-teacher-top.js
+++ page-teacher-top.js
@@ -290,6 +290,12 @@ main_db["日付"].forEach((element,index)=>{
       option.setAttribute("value",element);
       option.textContent = element;
       target1.getElementsByClassName("in-time")[0].appendChild(option);
+    });
+    var values = ["","8:30", "9:00", "9:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00"];
+    values.forEach(element =>{
+      var option = document.createElement('option');
+      option.setAttribute("value",element);
+      option.textContent = element;
       target1.getElementsByClassName("in-change")[0].appendChild(option);
     });
 
