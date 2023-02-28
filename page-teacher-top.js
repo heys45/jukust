ebafc4f71bc5b812ec62ecd4138fa0fd9c6fc1f5
ryohey <ryohey0405@gmail.com>
2023-02-28 15:01:11 +0900

    k

--- page-teacher-top.js
+++ page-teacher-top.js
@@ -339,6 +339,10 @@ senkou_db["TimeRex入力済"].forEach((element,index)=>{
     TimeRex.innerHTML="TimerexCalendar();"
     area_target.appendChild(TimeRex);
 
+    var TimeRexdata = document.getElementById("timerex_calendar")
+    area_target.appendChild(TimeRexdata);
+
+
   };
 });
 
