e0fab0c0d7975029060a2ad7aaa588d9a04eb840
ryohey <ryohey0405@gmail.com>
2023-03-03 06:53:13 +0900

    a

--- s-teacher.js
+++ s-teacher.js
@@ -98,9 +98,10 @@ for( var i=0; i<tables.length; i++) {
   document.getElementById("table-9").classList.add("col-header,row-header");
   document.getElementById("table-10").style.width="100%";
   document.getElementById("table-11").style.width="100%";
-  
+
+
   var slide_guide = document.createElement("p");
-  slide_guide.textContent = "表を左にスライドしてください";
+  slide_guide.innerHTML = "表を左にスライドしてください";
   slide_guide.style.color="red";
   var user =document.getElementById("ユーザープロフィール");
   console.log(user);
