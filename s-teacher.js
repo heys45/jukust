04e7041c93a3a0490e73f25dcdda0c642a796e31
ryohey <ryohey0405@gmail.com>
2023-03-03 06:35:31 +0900

    a

--- s-teacher.js
+++ s-teacher.js
@@ -103,7 +103,7 @@ for( var i=0; i<tables.length; i++) {
   slide_guide.textContent = "表を左にスライドしてください";
   slide_guide.style.color="red";
   
-  document.getElementById("ユーザープロフィール").after(slide-guide);
+  document.getElementById("ユーザープロフィール").after(slide_guide);
   document.getElementById("指導可能科目").after(slide-guide);
 
 
