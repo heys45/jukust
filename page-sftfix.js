245490d709c90eee2d468cba39dd078d8aa69b1d
ryohey <ryohey0405@gmail.com>
2023-03-02 13:16:18 +0900

    a

--- page-sftfix.js
+++ page-sftfix.js
@@ -96,9 +96,14 @@ var check_area = document.getElementById("申請が来たシフト一覧").nextE
     });
     console.log(make_db);}
 // #endregion
+
+
 var shift_db = make_db;
 // これで、shift_db[カラム名]で回答の配列を取得可能。
-console.log(shift_db["ステータス"][0])；
+console.log(shift_db["ステータス"][0]);
+console.log(shift_db["ステータス"][1]);
+console.log(shift_db["ステータス"][2]);
+console.log(shift_db["ステータス"]);
 
 
 //     //DB書き換え処理
