c48e1fee86f3803c714cb7d89f9eb97aca531692
ryohey <ryohey0405@gmail.com>
2023-03-03 04:27:10 +0900

    a

--- t-prof.js
+++ t-prof.js
@@ -103,8 +103,8 @@ for( var i=0; i<tables.length; i++) {
   slide_guide.textContent = "表を左にスライドしてください";
   slide_guide.style.color("red");
   
-  document.getElementById("ユーザープロフィール").after(slide-guide);
-  document.getElementById("指導可能科目").after(slide-guide);
+  // document.getElementById("ユーザープロフィール").after(slide-guide);
+  // document.getElementById("指導可能科目").after(slide-guide);
 
 
 
