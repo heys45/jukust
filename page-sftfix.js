dfe5b148a02395ea3561f84500694b29efcde6f4
ryohey <ryohey0405@gmail.com>
2023-03-02 15:26:23 +0900

    a

--- page-sftfix.js
+++ page-sftfix.js
@@ -181,7 +181,7 @@ forms.forEach((element,index) =>{
             var time2 = shift_db["終了時刻"][index];
             if(shift_db["備考・補足"][index] !=null){
             var hosoku = "<br>補足事項｜"+shift_db["備考・補足"][index];}
-            else{var hosoku = null}
+            else{var hosoku = ""}
             var shift_guide = "シフト時間｜　"+time1+time2+hosoku;
       
             // データがあるときの表示設定
@@ -191,22 +191,22 @@ forms.forEach((element,index) =>{
             target.getElementsByClassName("sch-info")[0].remove();
             target.getElementsByClassName("day-box-swich")[0].remove();
             }else{ //データがないときの表示設定　＊キーかつ講師回答のデータがない
-            // target.getElementsByClassName("day-box-swich")[0].remove();
-            // target.getElementsByClassName("sh-info")[0].remove();
-            // target.getElementsByClassName("sch-info")[0].before(target.getElementsByClassName("day-box-form")[0]);
-            // target.style.display="none";
+            target.getElementsByClassName("day-box-swich")[0].remove();
+            target.getElementsByClassName("sh-info")[0].remove();
+            target.getElementsByClassName("sch-info")[0].before(target.getElementsByClassName("day-box-form")[0]);
+            target.style.display="none";
             }
         }else{ //データがないときの表示設定　＊キーかつ講師回答のデータがない
-      //     target.getElementsByClassName("day-box-swich")[0].remove();
-      //     target.getElementsByClassName("sh-info")[0].remove();
-      //     target.getElementsByClassName("sch-info")[0].before(target.getElementsByClassName("day-box-form")[0]);
-      //     target.style.display="none";
+          target.getElementsByClassName("day-box-swich")[0].remove();
+          target.getElementsByClassName("sh-info")[0].remove();
+          target.getElementsByClassName("sch-info")[0].before(target.getElementsByClassName("day-box-form")[0]);
+          target.style.display="none";
       }});
     }  //データがないときの表示設定　＊キーかつ講師回答のデータがない
-    // target.getElementsByClassName("day-box-swich")[0].remove();
-    // target.getElementsByClassName("sh-info")[0].remove();
-    // target.getElementsByClassName("sch-info")[0].before(target.getElementsByClassName("day-box-form")[0]);
-    // target.style.display="none";
+    target.getElementsByClassName("day-box-swich")[0].remove();
+    target.getElementsByClassName("sh-info")[0].remove();
+    target.getElementsByClassName("sch-info")[0].before(target.getElementsByClassName("day-box-form")[0]);
+    target.style.display="none";
     
     // 参考ボックスの非表示
     target.getElementsByClassName("ajs-info")[0].remove();
