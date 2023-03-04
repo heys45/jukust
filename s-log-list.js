442859e8f94ac732744771c1ee6a90ed19478aa9
ryohey <ryohey0405@gmail.com>
2023-03-04 17:15:19 +0900

    a

--- s-log-list.js
+++ s-log-list.js
@@ -19,8 +19,8 @@ if(targets.includes(prop_object["ページ表示名"])){
   var img_element = document.createElement('img');
   img_element.src = 'https://heys45.github.io/jukust/logo1.png'; // 画像パス
   img_element.style.width="100%";
-  img_element.style["z-index"]="1000";
   var img_area=  document.querySelectorAll(".super-navbar__content span")[0];
+  document.querySelectorAll(".super-navbar__content img")[0].remove();
   img_area.appendChild(img_element);
 
 
