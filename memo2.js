34636862d86b0441fc2ada0f0496390d5897c8a9
ryohey <ryohey0405@gmail.com>
2023-03-04 21:47:35 +0900

    a

--- memo2.js
+++ memo2.js
@@ -48,3 +48,28 @@ main_db["日付"].forEach((element,index)=>{
     var counter = counter+1;
 
   }
+
+
+
+<div style="font-family: -apple-system, BlinkMacSystemFont, Roboto, &quot;Segoe UI&quot;, &quot;Fira Sans&quot;, Avenir, &quot;Helvetica Neue&quot;, &quot;Lucida Grande&quot;, sans-serif; height: 100vh; text-align: center; display: flex; flex-direction: column; align-items: center; justify-content: center;">
+  <div>
+    <style>
+                body { margin: 0; color: #000; background: #fff; }
+                .next-error-h1 {
+                  border-right: 1px solid rgba(0, 0, 0, .3);
+                }
+
+                @media (prefers-color-scheme: dark) {
+                  body { color: #fff; background: #000; }
+                  .next-error-h1 {
+                    border-right: 1px solid rgba(255, 255, 255, .3);
+                  }
+                }</style>
+              
+    <div style="display: inline-block; text-align: left; line-height: 49px; height: 49px; vertical-align: middle;">
+    <h2 style="font-size: 14px; font-weight: normal; line-height: 49px; margin: 0px; padding: 0px;">
+        Application error: a client-side exception has occurred (see the browser console for more information).
+    </h2>
+    </div>
+    </div>
+    </div>
