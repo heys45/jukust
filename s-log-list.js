9e9d3fe797ae501c3a4a99950249e52f8b50b672
ryohey <ryohey0405@gmail.com>
2023-03-04 17:28:38 +0900

    a

--- s-log-list.js
+++ s-log-list.js
@@ -27,8 +27,8 @@ if(targets.includes(prop_object["ページ表示名"])){
 }
 
 // ロゴの表示設定
-pc_logo.style.width="100%";pc_logo.setAttribute("id","pc_logo");pc_logo.style.height="32px";
-pc_logo.style.width="100%";pc_logo.setAttribute("id","sp_logo");pc_logo.style.height="32px";
+pc_logo.style.width="100%";pc_logo.setAttribute("id","pc_logo");pc_logo.style.height="32px";pc_logo.style.position="relative";
+pc_logo.style.width="100%";pc_logo.setAttribute("id","sp_logo");sp_logo.style.height="32px";sp_logo.style.position="relative";
 var img_area=  document.querySelectorAll(".super-navbar__content span")[0];
 document.querySelectorAll(".super-navbar__content img")[0].remove();
 img_area.appendChild(pc_logo);
