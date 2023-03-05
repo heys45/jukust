0e4d6a456b0b0e48db3ebcc9f5bf47569d7179e7
ryohey <ryohey0405@gmail.com>
2023-03-05 18:12:05 +0900

    a

--- s-message.js
+++ s-message.js
@@ -16,7 +16,7 @@ wr_form.remove();
 
 
 
-document.getElementsByClassName("notion-callout__content")[0].innerHTML.remove()
+document.getElementsByClassName("notion-callout__content")[0].removeChild();
 document.getElementsByClassName("notion-callout__content")[0].innerHTML=document.getElementsByClassName("notion-header__title")[0].innerHTML;
 
 
