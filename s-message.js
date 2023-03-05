9937909d60be125792c12232bdfcfa03a56d1c06
ryohey <ryohey0405@gmail.com>
2023-03-05 19:45:06 +0900

    a

--- s-message.js
+++ s-message.js
@@ -87,9 +87,9 @@ submit_button.setAttribute("type","submit");
 submit_button.setAttribute("form","message_form");
 var button_area= document.getElementsByClassName("notion-callout__icon")[1];
 button_area.appendChild(submit_button);
-console.log(button_area.getElementsByTagName("img")[1]);
-console.log(button_area.getElementsByTagName("picture")[1]);
-var button_icon=button_area.getElementsByTagName("img")[1];
+console.log(button_area.querySelectorAll("img")[1]);
+console.log(button_area.querySelectorAll("picture")[1]);
+var button_icon=button_area.querySelectorAll("img")[1];
 submit_button.appendChild(button_icon);
 
 // フォーム送信後の画面遷移の設定
