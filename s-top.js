aefa2355abedf68e4fa6f7bb1a32075c942c662d
ryohey <ryohey0405@gmail.com>
2023-03-05 19:43:10 +0900

    a

--- s-top.js
+++ s-top.js
@@ -20,6 +20,63 @@ if(prop_object["ページ表示名"]==="教室トップページ"){
   wr_form.remove();
 
 
+ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+DB表示設定代項目
+ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+
+var check_areas=[];
+var check_area = document.getElementsByClassName("notion-collection")[2];
+check_areas.push(check_area)
+var check_area = document.getElementsByClassName("notion-collection")[3];
+check_areas.push(check_area)
+var check_area = document.getElementsByClassName("notion-collection")[4];
+check_areas.push(check_area)
+var check_area = document.getElementsByClassName("notion-collection")[5];
+check_areas.push(check_area)
+
+
+
+// #region データベース個別処理
+for (let index = 0; index < check_areas.length; index++) {
+
+  if(check_areas[index].getElementsByClassName("notion-collection-table")[0] !=null){
+    var check_db =check_areas[index].getElementsByClassName("notion-collection-table") [0];
+    var make_db ={};
+    var headers = check_db.querySelectorAll("thead th");
+    headers.forEach(element =>{
+      make_db[element.innerHTML]=[];
+    });
+    var datas = check_db.querySelectorAll("tbody tr");
+    datas.forEach(element =>{
+      var datas = element.querySelectorAll("td");
+      for (let i = 0; i< datas.length; i++) {
+        if(datas[i].querySelectorAll("span")[0] !=null){
+          if(datas[i].querySelectorAll("span span")[0] !=null){
+            var data = datas[i].querySelectorAll("span span")[0].innerHTML;}
+          else{
+            var data = datas[i].querySelectorAll("span")[0].innerHTML;}
+        }else{var data=null;}
+        make_db[headers[i].innerHTML].push(data);}
+    });
+    console.log(make_db);}
+// #endregion
+if(make_db !=null){
+var message_db = make_db;}
+console.log(message_db);
+
+if(check_areas[index].getElementsByClassName("notion-collection-table")[0] !=null){
+  var check_db =check_areas[index].getElementsByClassName("notion-collection-table") [0];
+  var datas = check_db.querySelectorAll("tbody tr");
+  datas.forEach((element,index) =>{
+    console.log(message_db["教室ID"][index]!=prop_object["教室ID"])
+      if(message_db["教室ID"][index]!=prop_object["教室ID"]){
+        element.style.display="none"
+      }
+  });
+}
+
+  
+}
 
 
 
