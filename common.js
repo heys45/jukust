001dbd29060d4c0da578367ffe5a4f02368d6919
ryohey <ryohey0405@gmail.com>
2023-03-05 17:02:36 +0900

    a

--- common.js
+++ common.js
@@ -75,43 +75,43 @@ remove_dbg.forEach(element =>{element.remove();});
 // DB表示設定代項目
 //ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 
-var check_area = document.getElementById("割り当て講師の一覧").nextElementSibling.nextElementSibling;
-// #region データベース個別処理
-  if(check_area.getElementsByClassName("notion-collection-table")[0] !=null){
-    var check_db =check_area.getElementsByClassName("notion-collection-table") [0];
-    var make_db ={};
-    var headers = check_db.querySelectorAll("thead th");
-    headers.forEach(element =>{
-      make_db[element.innerHTML]=[];
-    });
-    var datas = check_db.querySelectorAll("tbody tr");
-    datas.forEach(element =>{
-      var datas = element.querySelectorAll("td");
-      for (let i = 0; i< datas.length; i++) {
-        if(datas[i].querySelectorAll("span")[0] !=null){
-          if(datas[i].querySelectorAll("span span")[0] !=null){
-            var data = datas[i].querySelectorAll("span span")[0].innerHTML;}
-          else{
-            var data = datas[i].querySelectorAll("span")[0].innerHTML;}
-        }else{var data=null;}
-        make_db[headers[i].innerHTML].push(data);}
-    });
-    console.log(make_db);}
-// #endregion
-if(make_db !=null){
-var message_db = make_db;}
-console.log(message_db);
+// var check_area = document.getElementById("割り当て講師の一覧").nextElementSibling.nextElementSibling;
+// // #region データベース個別処理
+//   if(check_area.getElementsByClassName("notion-collection-table")[0] !=null){
+//     var check_db =check_area.getElementsByClassName("notion-collection-table") [0];
+//     var make_db ={};
+//     var headers = check_db.querySelectorAll("thead th");
+//     headers.forEach(element =>{
+//       make_db[element.innerHTML]=[];
+//     });
+//     var datas = check_db.querySelectorAll("tbody tr");
+//     datas.forEach(element =>{
+//       var datas = element.querySelectorAll("td");
+//       for (let i = 0; i< datas.length; i++) {
+//         if(datas[i].querySelectorAll("span")[0] !=null){
+//           if(datas[i].querySelectorAll("span span")[0] !=null){
+//             var data = datas[i].querySelectorAll("span span")[0].innerHTML;}
+//           else{
+//             var data = datas[i].querySelectorAll("span")[0].innerHTML;}
+//         }else{var data=null;}
+//         make_db[headers[i].innerHTML].push(data);}
+//     });
+//     console.log(make_db);}
+// // #endregion
+// if(make_db !=null){
+// var message_db = make_db;}
+// console.log(message_db);
 
-if(check_area.getElementsByClassName("notion-collection-table")[0] !=null){
-  var check_db =check_area.getElementsByClassName("notion-collection-table") [0];
-  var datas = check_db.querySelectorAll("tbody tr");
-  datas.forEach((element,index) =>{
-    console.log(message_db["教室ID"][index]!=prop_object["教室ID"])
-      if(message_db["教室ID"][index]!=prop_object["教室ID"]){
-        element.style.display="none"
-      }
-  });
-}
+// if(check_area.getElementsByClassName("notion-collection-table")[0] !=null){
+//   var check_db =check_area.getElementsByClassName("notion-collection-table") [0];
+//   var datas = check_db.querySelectorAll("tbody tr");
+//   datas.forEach((element,index) =>{
+//     console.log(message_db["教室ID"][index]!=prop_object["教室ID"])
+//       if(message_db["教室ID"][index]!=prop_object["教室ID"]){
+//         element.style.display="none"
+//       }
+//   });
+// }
 
 
 
