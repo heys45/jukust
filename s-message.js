62934f3dd9acd48be190bfd97c9fc04085f7479b
ryohey <ryohey0405@gmail.com>
2023-03-05 17:23:03 +0900

    a

--- s-message.js
+++ s-message.js
@@ -19,7 +19,8 @@ var log_box =document.createElement("div");
 log_box.innerHTML='メッセージのエリア';
 log_box.style.border="solid 2px #555555"
 var area = document.querySelectorAll(".notion-callout:first-child")[0];
-area.appendChild(log_box);
+area.after(log_box);
+
 
 
 
@@ -33,6 +34,67 @@ message_box.innerHTML=''+
 '</form>';
 var area = document.querySelectorAll(".notion-callout:nth-child(2)")[0];
 area.appendChild(message_box);
+
+
+var check_area = document.querySelectorAll(".notion-collection-group__table:nth-child(3)")[0];
+// #region データベース個別処理
+  if(check_area.getElementsByClassName("notion-collection-table")[0] !=null){
+    var check_db =check_area.getElementsByClassName("notion-collection-table") [0];
+    var make_db ={};
+    var headers = check_db.querySelectorAll("thead th");
+    headers.forEach(element =>{
+      make_db[element.innerHTML]=[];
+    });
+    var datas = check_db.querySelectorAll("tbody tr");
+    datas.forEach(element =>{
+      var datas = element.querySelectorAll("td");
+      for (let i = 0; i< datas.length; i++) {
+        if(datas[i].querySelectorAll("span")[0] !=null){
+          if(datas[i].querySelectorAll("span span")[0] !=null){
+            var data = datas[i].querySelectorAll("span span")[0].innerHTML;}
+          else{
+            var data = datas[i].querySelectorAll("span")[0].innerHTML;}
+        }else{var data=null;}
+        make_db[headers[i].innerHTML].push(data);}
+    });
+    console.log(make_db);}
+// #endregion
+if(make_db !=null){
+var message_db = make_db;}
+console.log(message_db);
+
+if(check_area.getElementsByClassName("notion-collection-table")[0] !=null){
+  var check_db =check_area.getElementsByClassName("notion-collection-table") [0];
+  var datas = check_db.querySelectorAll("tbody tr");
+  datas.forEach((element,index) =>{
+    console.log(message_db["応募ID"][index]!=prop_object["応募ID"])
+      if(message_db["応募ID"][index]!=prop_object["応募ID"]){
+
+        var message_data_box = document.createElement("div");
+        message_box.style.display="flex";
+        message_box.style.flexWrap="wrap"
+        var message_data1= document.createElement("span");
+        message_data1.innerHTML=message_db["送信時間"][index]+"｜"+message_db["教室ID"][index]+message_db["会員ID"][index];
+        message_data1.style.display="inline-box";
+        message_data1.style.width="80%";
+        message_data1.style.margin="3px 15px"
+        message_data_box.appendChild(message_data1);
+        var message_data2= document.createElement("div");
+        message_data2.innerHTML=message_db["メッセージ"][index]
+        message_data2.style.width="80%"
+        message_data2.style.margin="3px 15px"
+        message_data2.style.padding="10pxpx 10px"
+        message_data2.style.backgroundColor="#cccccc";
+        message_data2.style.borderRadius="5px"
+        message_box.appendChild(message_data2);
+        if(message_db["教室ID"][index]!=null){
+          message_data2.style.justifyContent="flex-end"
+          message_data2.style.backgroundColor="#aaaaaa";
+        }
+        log_box.appendChild(message_data_box);
+      }
+  });
+}
 
 
 
