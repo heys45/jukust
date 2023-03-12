8b39f027f2bb6f863b3813f53904707368055bfd
ryohey <ryohey0405@gmail.com>
2023-03-12 12:37:41 +0900

    a

--- common.js
+++ common.js
@@ -1,6 +1,6 @@
 setTimeout(timefunc,1000);
 console.log("common.js")
-alert( "ページ名のリンク変更テスト1235" );
+alert( "ページ名のリンク変更テスト1237" );
 
 // データベースの集合と
 var dbs= document.querySelectorAll(".notion-collection");
