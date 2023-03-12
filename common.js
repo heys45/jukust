0572661c5866cbeb8b6f375ede94574b54fd6f10
ryohey <ryohey0405@gmail.com>
2023-03-13 07:21:45 +0900

    a

--- common.js
+++ common.js
@@ -169,5 +169,7 @@ var rowdatas = page_list_db.querySelectorAll("tbody tr");
   for (let i =OFF_number ; i< datas.length; i++) {
     headers0[i].style.display="none"
   }
+  // page_listを表示してあげる
+  document.querySelectorAll(".notion-column-list .notion-column .notion-collection:first-child")[0].style.display="block";
   
 }
