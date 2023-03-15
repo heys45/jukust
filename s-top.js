84172d3bc5e69a285d5e701442549b34e6220ce0
ryohey <ryohey0405@gmail.com>
2023-03-16 06:11:22 +0900

    a

--- s-top.js
+++ s-top.js
@@ -55,7 +55,7 @@ var rowdatas = sf_list_db.querySelectorAll("tbody tr");
 // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 // #region
 // kt_listのデータ取得。
-var kt_list_db = document.querySelectorAll(".notion-collection-table".slice(-1)[0];
+var kt_list_db = document.querySelectorAll(".notion-collection-table").slice(-1)[0];
 var kt_list ={};
 var headers0 = kt_list_db.querySelectorAll("thead th");
 var headers = []
